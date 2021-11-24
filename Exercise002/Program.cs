42c7f82530ba17e8fd13524aa6d6da4998cfce4b
Alexander <nikolov7@vk.com>
2021-11-24 23:15:04 +0300

    did Exercise002

---
+++ Exercise002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Является ли первое квадратом второго?");
+Console.WriteLine("Enter number1:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number2:");
+int num2 = int.Parse(Console.ReadLine());
+    if(num1 == num2 * num2)
+      {
+             Console.WriteLine("Да");
+      }
+    else
+     {
+             Console.WriteLine("Нет");
+     }
