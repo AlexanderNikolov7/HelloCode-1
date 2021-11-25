5723c853c685514c02713fa0ec98fbaaf45bdec7
Alexander <nikolov7@vk.com>
2021-11-25 13:52:54 +0300

    did Exercise005

---
+++ Exercise005/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Найти максимальное из трех чисел");
+int num1 = 154454;
+int num2 = 68464;
+int num3 = 56628;
+int max = num1;
+if(num2 > max) max = num2;
+if(num3 > max) max = num3;
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
