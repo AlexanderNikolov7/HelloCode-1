0f7151f9d237d3683cca19a161fb3b183eaefd5e
Alexander <nikolov7@vk.com>
2021-11-25 10:53:02 +0300

    did Exercise003

---
+++ Exercise003/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Даны два числа. Показать большее и меньшее число");
+Console.WriteLine("Enter number1:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number2:");
+int num2 = int.Parse(Console.ReadLine());
+    if(num1 > num2)
+      {
+             Console.Write(num1);
+             Console.Write(" Больше чем ");
+             Console.Write(num2);
+      }
+    else
+     {
+             Console.Write(num2);
+             Console.Write(" Больше чем ");
+             Console.Write(num1);
+     }
