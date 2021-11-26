f5189eb658a99649a9746984d83f5f45c1d435c3
Alexander <nikolov7@vk.com>
2021-11-26 14:02:42 +0300

    did Exercise006

---
+++ Exercise006/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Написать программу вычисления значения функции y = f(a)");
+// f(a) = a^2 + 1
+Console.WriteLine("Введите значение a: ");
+int a = int.Parse(Console.ReadLine());
+int y = a * a + 1;
+Console.Write("y = ");
+Console.WriteLine(y);
+
