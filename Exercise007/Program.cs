a1b796bdf6d85e8ca6ff95f387d63c09f4bc0611
Alexander <nikolov7@vk.com>
2021-11-26 22:11:10 +0300

    did Exercise007

---
+++ Exercise007/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Показать числа от -N до N");
+// числа от -N до N = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4}
+int[] array = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4};
+
+int n = array.Length;
+int index = 0;
+    while (index < n)
+    {
+        if (index < n)
+            {
+                Console.WriteLine(array[index]);
+            }
+            index++;
+    }
