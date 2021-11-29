cad4785482733f1df61ae0069d8525880ae1f60c
Alexander <nikolov7@vk.com>
2021-11-29 17:46:57 +0300

    did Exercise010

---
+++ Exercise010/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Показать вторую цифру трёхзначного числа");
+Console.WriteLine("Введите трёхзначное число: ");
+string num = Console.ReadLine();
+Console.Write("Вторая цифра трёхзначного числа = ");
+Console.WriteLine(num[1]);
