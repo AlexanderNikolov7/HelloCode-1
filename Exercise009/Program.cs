3666ddd1c68f5872cd24cc55a7e794dcee09f09b
Alexander <nikolov7@vk.com>
2021-11-29 17:35:21 +0300

    did Exercise009

---
+++ Exercise009/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Показать последнюю цифру трёхзначного числа");
+Console.WriteLine("Введите трёхзначное число: ");
+string num = Console.ReadLine();
+Console.Write("Последняя цифра трёхзначного числа = ");
+Console.WriteLine(num[2]);
