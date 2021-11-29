7da495a1fba76a893610b07630fdfc7f607b5002
Alexander <nikolov7@vk.com>
2021-11-29 16:23:04 +0300

    did Exercise008

---
+++ Exercise008/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Показать четные числа от 1 до N");
+// пусть числа от одношо до N = {1,2,3,4,5,6,7,8,9,10}
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+for (int index = 0; index < array.Length; index++)
+{
+    if (array[index] % 2 == 0)
+        Console.WriteLine(array[index]);
+}
