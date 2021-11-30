0a4b2df065148524d9bf9df82b621372b98680a6
Alexander <nikolov7@vk.com>
2021-11-30 11:41:59 +0300

    did Exercise013

---
+++ Exercise013/Program.cs
@@ -0,0 +1,14 @@
+﻿//Console.WriteLine("Выяснить, кратно ли число заданному, если нет, вывести остаток.");
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 % num2 == 0)
+{
+    Console.WriteLine($"{num1} {"является кратным"} {num2}");
+}    
+else
+{
+    int res = num1 % num2;
+    Console.WriteLine($"{"Остаток равен: "}{res}");
+}
