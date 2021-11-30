b95df5530316bc0657e31ce154e91064c32371da
Alexander <nikolov7@vk.com>
2021-11-30 12:04:10 +0300

    did Exercise014

---
+++ Exercise014/Program.cs
@@ -0,0 +1,11 @@
+﻿//Console.WriteLine("Найти третью цифру числа или сообщить, что её нет");
+Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+if (num.Length < 3)
+{
+    Console.WriteLine("Третьей цифры числа, нет!");
+}
+else
+{
+    Console.WriteLine($"{"Третья цифра числа равна: "}{num[2]}");
+}
