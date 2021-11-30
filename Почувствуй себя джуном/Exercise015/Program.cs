2cff992ed1c69d134785c2cdc1ff257bd5634a22
Alexander <nikolov7@vk.com>
2021-11-30 12:32:33 +0300

    did Exercise015

---
+++ Почувствуй себя джуном/Exercise015/Program.cs
@@ -0,0 +1,19 @@
+﻿//Console.WriteLine("Дано число. Проверить кратно ли оно 7 и 23");
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if(num % 7 == 0) 
+{
+    Console.WriteLine($"{"Число "}{num} {"кратно 7"}");
+}
+else
+{
+    Console.WriteLine($"{"Число "}{num} {"не является кратным 7"}");
+}    
+if(num % 23 == 0) 
+{
+    Console.WriteLine($"{"Число "}{num} {"кратно 23"}");
+}
+else
+{
+    Console.WriteLine($"{"Число "}{num} {"не является кратным 23"}");
+}    
