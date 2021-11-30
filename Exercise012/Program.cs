ba7d4c9b8eff147a16f45421c2def1a03eb67515
Alexander <nikolov7@vk.com>
2021-11-30 11:07:02 +0300

    did Exercise012

---
+++ Exercise012/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Удалить вторую цифру трёхзначного числа");
+Console.WriteLine("Введите трёхзначное число: ");
+string num = Console.ReadLine();
+Console.WriteLine($"{"Результат: "}{num[0]}{num[2]}");
