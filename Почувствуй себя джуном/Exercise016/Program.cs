ff00ab57431554272589c6e0326272d6ceafa56b
Alexander <nikolov7@vk.com>
2021-11-30 12:51:02 +0300

    did Exercise016

---
+++ Почувствуй себя джуном/Exercise016/Program.cs
@@ -0,0 +1,10 @@
+﻿//Console.WriteLine("Дано число обозначающее день недели. Выяснить является номер дня недели выходным");
+Console.Write("Введите число дня недели: ");
+int num = int.Parse(Console.ReadLine());
+if(num == 6) Console.WriteLine($"{num}{" день недели - является выходным"}");
+if(num == 7) Console.WriteLine($"{num}{" день недели - является выходным"}");
+if(num > 7) Console.WriteLine($"{num}{" - это число не является днём недели"}");
+else
+{
+    Console.WriteLine($"{num}{" день недели - не является выходным"}");
+}
