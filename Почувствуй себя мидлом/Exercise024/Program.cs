e1a7efe368bb872ba83c8bd0cbf832a18d892638
Alexander <nikolov7@vk.com>
2021-12-03 21:15:52 +0300

    did Exercise024

---
+++ Почувствуй себя мидлом/Exercise024/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+Console.Write("Введиту значение N: ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    double res = Math.Pow(i, 3);
+    Console.WriteLine($"{i}^3 = {res}");
+}
+
