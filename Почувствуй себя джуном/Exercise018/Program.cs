8472fc918f044de6e0bf69d2215de0d9b01608a4
Alexander <nikolov7@vk.com>
2021-12-03 15:01:22 +0300

    did Exercise018

---
+++ Почувствуй себя джуном/Exercise018/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y");
+bool X = true;
+bool Y = true;
+ 
+bool XorY = X | Y; 
+bool notXorY = !XorY;
+
+bool notX = !X;
+bool notY = !Y;
+bool notXandNotY = notX & notY;
+
+bool result = notXorY == notXandNotY;
+Console.WriteLine(result);
