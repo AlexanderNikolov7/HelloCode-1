8bd15396d04ab3f28d080bbd5ba4fc2bfa00153c
Alexander <nikolov7@vk.com>
2021-12-03 20:58:44 +0300

    did Exercise023

---
+++ Почувствуй себя мидлом/Exercise023/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать таблицу квадратов чисел от 1 до N 
+Console.WriteLine("Введиту значение N: ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"{i}^2 = {i*i}");
+}
