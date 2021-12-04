0f81883ef266c5ba408c089362cc911d97b10739
Alexander <nikolov7@vk.com>
2021-12-04 18:27:26 +0300

    did Exercise026

---
+++ Почувствуй себя мидлом/Exercise026/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.Write("Введиту значение числа A: ");
+double A = int.Parse(Console.ReadLine());
+Console.Write("Введиту значение степени B: ");
+double B = int.Parse(Console.ReadLine());
+double sum = 1;
+for (int i = 0; i < B; i++)
+{
+    sum = sum * A;
+}
+Console.WriteLine($"{"A^B"} = {sum}");
