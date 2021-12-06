cb7e93a0110012de941a52d5399a12778d30dc7c
Alexander <nikolov7@vk.com>
2021-12-06 22:27:16 +0300

    did Exercise027

---
+++ Почувствуй себя мидлом/Exercise027/Program.cs
@@ -0,0 +1,9 @@
+﻿// Определить количество цифр в числе
+Console.Write("Ввудите любое число: ");
+string num = Console.ReadLine();
+int numbers = 0;
+for (int i = 0; i < num.Length; i++)
+{
+    numbers++;
+}
+Console.WriteLine($"Количество цифр в числе {num} = {numbers}");
