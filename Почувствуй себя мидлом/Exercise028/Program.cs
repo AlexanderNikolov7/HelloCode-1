16d90bbba5a40695687e20d9a34ef213e3f3911d
Alexander <nikolov7@vk.com>
2021-12-07 09:45:18 +0300

    did Exercise028

---
+++ Почувствуй себя мидлом/Exercise028/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+int num1 = num;
+while (num > 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+}
+Console.WriteLine($"Cумму цифр в числе {num1} = {sum}");
