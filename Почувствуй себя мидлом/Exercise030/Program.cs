195d077d2a1a9b919589feb4ed969097d9852b02
Alexander <nikolov7@vk.com>
2021-12-07 15:02:11 +0300

    did Exercise030

---
+++ Почувствуй себя мидлом/Exercise030/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Введите количество случайных чисел: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 99);
+    Console.Write($"{array[i]}, ");
+}
+Console.WriteLine();
+// Получился массив с случайно сгкенерированными числами
+// Осталось найти чётные числа и возвести их в кубы
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0) Console.Write($"{array[i]}^3 = {Math.Pow(array[i], 3)}; ");
+}
+
