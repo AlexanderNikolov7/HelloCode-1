a3a60375be36048d6909c92376c1fd52575a3f63
Alexander <nikolov7@vk.com>
2021-12-07 10:33:02 +0300

    did Exercise029

---
+++ Почувствуй себя мидлом/Exercise029/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Факториал числа {n} = {Factorial(n)}");
