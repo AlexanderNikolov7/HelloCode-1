4256c299c6887916d39fc38c74fcc8365afd25af
Alexander <nikolov7@vk.com>
2021-12-07 21:03:26 +0300

    did Exercise031

---
+++ Почувствуй себя сеньором/Exercise031/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать массив из 8 элементов и вывести их на экран 
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+}
+
+int[] collection = new int[8];
+FillArray(collection);
+PrintArray(collection);
