f4d6e08997bb76097eb620cf9729dd637aa70299
Alexander <nikolov7@vk.com>
2021-12-08 14:28:30 +0300

    did Exercise034

---
+++ Почувствуй себя сеньором/Exercise034/Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу замену элементов массива на противоположные
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void Replacement(int[] array)
+{
+    int number = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        number = array[i] * -1;
+        Console.Write($"{number} ");
+    }
+}
+
+int[] collection = new int[10];
+FillArray(collection);
+PrintArray(collection);
+Console.WriteLine();
+Replacement(collection);
