f66cfd076f9e1e4eb7f8051e9e0e74653c63d685
Alexander <nikolov7@vk.com>
2021-12-08 13:42:21 +0300

    did Exercise033

---
+++ Почувствуй себя сеньором/Exercise033/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+}
+
+void CalculateSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum = sum + array[i];
+    }
+    Console.WriteLine($"Сумма элементов этого массива = {sum}");
+}
+
+int[] collection = new int[12];
+FillArray(collection);
+PrintArray(collection);
+Console.WriteLine();
+CalculateSum(collection);
