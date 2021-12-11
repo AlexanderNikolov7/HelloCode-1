964d78e3e7c3cf1c3dc6d64ff824359b328eb98b
Alexander <nikolov7@vk.com>
2021-12-11 17:39:33 +0300

    did Exercise037

---
+++ Почувствуй себя сеньором/Exercise037/Program.cs
@@ -0,0 +1,24 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99].
+void PrintNewArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 1000);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void SegmentSearch(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 10 && array[i] < 100) count = count + 1;
+    }
+    Console.WriteLine($"Количество элементов из отрезка [10,99] = {count}");
+}
+
+int[] array = new int[123];
+PrintNewArray(array);
+Console.WriteLine();
+SegmentSearch(array);
