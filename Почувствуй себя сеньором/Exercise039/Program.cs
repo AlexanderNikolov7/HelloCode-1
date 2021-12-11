25cb16fb97384ae9fbb6b1d0c9d64cca9ea4df47
Alexander <nikolov7@vk.com>
2021-12-11 20:55:24 +0300

    did Exercise039

---
+++ Почувствуй себя сеньором/Exercise039/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+void PrintNewArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+void Result(int[] array)
+{
+    int count = 1;
+    int result = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array.Length / 2 == i) break;
+        result = array[i] * array[array.Length - count];
+        count++;
+        Console.Write($"{result} ");
+    }
+}
+
+int[] array = new int[10];
+PrintNewArray(array);
+Console.WriteLine();
+Result(array);
