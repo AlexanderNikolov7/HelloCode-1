b738de9b8657fdc54d241ac1b2fffe14a3e1126e
Alexander <nikolov7@vk.com>
2021-12-12 15:59:46 +0300

    did Exercise040

---
+++ Почувствуй себя сеньором/Exercise040/Program.cs
@@ -0,0 +1,33 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом.
+void PrintNewArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void DifferenceMaxMin(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (max < array[i]) max = array[i];
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Максимальный эдемент = {max}");
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min > array[i]) min = array[i];
+    }
+    Console.WriteLine($"Минимальный элемент = {min}");
+    double result = 0;
+    result = max - min;
+    Console.WriteLine($"Разницу между максимальным и минимальным элементом = {result}");
+}
+
+double[] array = new double[10];
+PrintNewArray(array);
+DifferenceMaxMin(array);
