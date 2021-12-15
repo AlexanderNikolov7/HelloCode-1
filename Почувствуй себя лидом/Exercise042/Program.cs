94474f558e610684c992c904830f535f84a4dbdd
Alexander <nikolov7@vk.com>
2021-12-15 14:39:01 +0300

    did Exercise042

---
+++ Почувствуй себя лидом/Exercise042/Program.cs
@@ -0,0 +1,11 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры.
+Console.WriteLine("Сколько чисел определить если они больше 0 ?");
+int count = int.Parse(Console.ReadLine());
+int numbers = 0;
+for (int i = 1; i < count + 1; i++)   
+{
+    Console.WriteLine($"Введите число № {i}");
+    int x = int.Parse(Console.ReadLine());
+    if (x > 0)numbers = numbers + 1;
+}
+Console.WriteLine($"{numbers} числа(ел) больше 0 введено с клавиатуры.");
