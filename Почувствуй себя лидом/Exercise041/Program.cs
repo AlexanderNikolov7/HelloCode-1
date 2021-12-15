9a125161f46d2d83cf2c1c967980e1484c8cdb0e
Alexander <nikolov7@vk.com>
2021-12-15 13:57:44 +0300

    did Exercise041

---
+++ Почувствуй себя лидом/Exercise041/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить являются ли три числа сторонами треугольника.
+// Формула: (a<b+c) and (b<a+c) and (c<a+b) = числа a,b,c являются сторонами треугольника.
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+if (a < (b + c) && b < (a + c) && c < (a + b)) Console.Write("числа a,b,c являются сторонами треугольника.");
+else
+{
+    Console.WriteLine("числа a,b,c не являются сторонами треугольника.");
+}
