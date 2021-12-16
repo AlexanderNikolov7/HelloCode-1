d0e75726dd47580884a64a87da8101d14c082042
Alexander <nikolov7@vk.com>
2021-12-16 15:45:59 +0300

    did Exercise043

---
+++ Почувствуй себя лидом/Exercise043/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу преобразования десятичного числа в двоичное.
+
+Console.Write("Decimal: ");
+int decimalNumber = int.Parse(Console.ReadLine());
+
+int remainder;
+string result = string.Empty;
+while (decimalNumber > 0)
+{
+    remainder = decimalNumber % 2;
+    decimalNumber /= 2;
+    result = remainder.ToString() + result; // цифры сохраняются с права на лево: 1 01 101 1101 =  1101
+}
+Console.WriteLine("Binary:  {0}",result);
