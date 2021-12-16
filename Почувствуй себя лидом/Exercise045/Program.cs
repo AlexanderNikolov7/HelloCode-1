6a0069f3f039620be35ceb601c60454f3d30cb34
Alexander <nikolov7@vk.com>
2021-12-16 18:01:50 +0300

    did Exercise045

---
+++ Почувствуй себя лидом/Exercise045/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать числа Фибоначчи.
+Console.Write("Введите число: ");
+double n = double.Parse(Console.ReadLine());
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < n + 1; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
