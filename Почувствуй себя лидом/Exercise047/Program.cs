ac6f1c684ace3a2aca9704adae09f457f20be16e
Alexander <nikolov7@vk.com>
2021-12-17 17:13:35 +0300

    did Exercise047

---
+++ Почувствуй себя лидом/Exercise047/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу копирования массива.
+int[] array = new int[10];
+int[] newArray = new int[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+    newArray[i] = array[i];
+    Console.WriteLine($"{array[i]} - {newArray[i]}");
+}
