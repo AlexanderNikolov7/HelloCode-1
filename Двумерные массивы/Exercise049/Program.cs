5b5f7b513ca129c5011fc4fd28432d583b0a11ab
Alexander <nikolov7@vk.com>
2021-12-18 13:29:59 +0300

    did Exercise049

---
+++ Двумерные массивы/Exercise049/Program.cs
@@ -0,0 +1,32 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами.
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+double[,] array = new double[m, n];
+FillArray(array);
+PrintArray(array);
