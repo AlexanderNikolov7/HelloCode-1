1eb330f2e57acbe262630c0a72d3f15bd5fe99f4
Alexander <nikolov7@vk.com>
2021-12-18 13:53:21 +0300

    did Exercise050

---
+++ Двумерные массивы/Exercise050/Program.cs
@@ -0,0 +1,41 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные.
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void Replace(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] % 2 == 0) array[i, j] = array[i, j] * -1;
+        }
+    }
+}
+
+int[,] array = new int[5, 10];
+FillArray(array);
+PrintArray(array);
+Replace(array);
+PrintArray(array);
