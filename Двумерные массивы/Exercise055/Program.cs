1d526d72301604a69ba3039b3e491bef22795591
Alexander <nikolov7@vk.com>
2021-12-19 20:11:51 +0300

    did Exercise055

---
+++ Двумерные массивы/Exercise055/Program.cs
@@ -0,0 +1,42 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Result(int[,] array, double columnSum)
+{
+    for (int columns = 0; columns < array.GetLength(1); columns++)
+    {
+        for (int rows = 0; rows < array.GetLength(0); rows++)
+        {
+            columnSum = columnSum + array[rows, columns];
+        }
+        Console.WriteLine($"Среднее арифметическое столбца {columns + 1} = {columnSum / array.GetLength(0)}");
+        columnSum = 0;
+    }
+}
+
+int[,] array = new int[3, 4];
+double columnSum = 0;
+FillArray(array);
+PrintArray(array);
+Result(array, columnSum);
