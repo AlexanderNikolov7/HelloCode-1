3ee8b3240610558f2fdb231b32e46f3af14040f7
Alexander <nikolov7@vk.com>
2021-12-28 12:19:37 +0300

    did Exercise068

---
+++ Рекурсия/Exercise068/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать натуральные числа от M до N, N и M заданы.
+
+string NumbersRec(int M, int N)
+{
+    if (M == N) return $"{M}";
+    else return $"{M} " + NumbersRec(M + 1, N);
+}
+int m = 1;
+int n = 10;
+Console.WriteLine(NumbersRec(m, n));
