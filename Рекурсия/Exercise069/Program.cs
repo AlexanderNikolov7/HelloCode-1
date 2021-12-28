54aee56d69ff81a6b6c8c53fedce90d22d4869fc
Alexander <nikolov7@vk.com>
2021-12-28 12:34:46 +0300

    did Exercise069

---
+++ Рекурсия/Exercise069/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму элементов от M до N, N и M заданы.
+
+int SumRec(int M, int N)
+{
+    if (M == N) return 0;
+    else return N + SumRec(M, N - 1);
+}
+int m = 1;
+int n = 10;
+Console.WriteLine(SumRec(m, n));
