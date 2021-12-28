d8f09b442170f634f11934c1f53b21b774327848
Alexander <nikolov7@vk.com>
2021-12-28 12:07:57 +0300

    did Exercise067

---
+++ Рекурсия/Exercise067/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать натуральные числа от N до 1, N задано.
+
+string NumbersRec(int N)
+{
+    if (N == 1) return String.Empty + N;
+    else return $"{N} " + NumbersRec(N - 1);
+}
+int N = 10;
+System.Console.WriteLine(NumbersRec(N));
